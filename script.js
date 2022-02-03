b5e013044dd403383f71da16c9c77da71eef5fe1
marikoabe <mariko.abe@gmail.com>
2022-02-02 22:59:15 -0800

    Buttons added to webpage, RPS now triggered by buttons

--- script.js
+++ script.js
@@ -6,70 +6,83 @@ function game() {
 
     let playerScore = 0;
     let computerScore = 0;
+    let playerSelection = "";
+    let computerSelection = computerPlay();
 
-    for (let i = 1; i < 6 ; i++) {
+    // Function that randomly returns "Rock", "Paper" or "Scissors" i.e. computer's hand
+    function computerPlay() {
+        plays = ["rock", "paper", "scissors"];
+        computerHand = plays[Math.floor(Math.random() * plays.length)];
+        // console.log(computerHand);
+        return computerHand;
+    };
 
-        // Function to intake player's hand, case insensitive
-        function playerPlay() {
-            playerHand = prompt("Rock, paper, scissors?").toLowerCase();
+    // Event listener for buttons to trigger round of RPS to start
+    const rockButton = document.querySelector("#rock-button");
+    const paperButton = document.querySelector("#paper-button");
+    const scissorsButton = document.querySelector("#scissors-button");
+    
+    rockButton.addEventListener("click", playerSelectionRock);
+    paperButton.addEventListener("click", playerSelectionPaper);
+    scissorsButton.addEventListener("click", playerSelectionScissors);
 
-            while (playerHand !== "rock" && playerHand !== "paper" && playerHand !== "scissors") {
-                playerHand = prompt("Input invalid; please pick from rock, paper or scissors.").toLowerCase();
-            }
+    function playerSelectionRock() {
+        playRound("rock", computerPlay());
+    }
+    function playerSelectionPaper() {
+        playRound("paper", computerPlay());
+    }
+    function playerSelectionScissors() {
+        playRound("scissors", computerPlay());
+    }
 
-            return playerHand;    
+    // Function to play a round of Rock, Paper, Scissors between player's hand and computer's hand, winner scores 1
+    function playRound (playerSelection, computerSelection) {
+        while (playerSelection === computerSelection){
+            alert("You tied with the computer, please try again.");
+            break;
         }
 
-        // Function that randomly returns "Rock", "Paper" or "Scissors" i.e. computer's hand
-        function computerPlay() {
-            plays = ["rock", "paper", "scissors"];
-            computerHand = plays[Math.floor(Math.random() * plays.length)];
-            return computerHand;
-        }
-
-        let playerSelection = playerPlay();
-        let computerSelection = computerPlay();
         console.log(playerSelection, computerSelection);
-
-        // Function to play a round of Rock, Paper, Scissors between player's hand and computer's hand, winner scores 1
-        function playRound (playerSelection, computerSelection) {
-            while (playerSelection === computerSelection){
-                alert("You tied with the computer, please try again.");
-                playerSelection = playerPlay();
-                computerSelection = computerPlay();
-                console.log(playerSelection, computerSelection);
-            }
-            
-            if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
-                let roundWinner = "player";
-                console.log(roundWinner);
-                alert("You win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection + ".");
-                playerScore += 1;
-            } else {
-                let roundWinner = "computer";
-                console.log(roundWinner);
-                alert("You lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection + ".");
-                computerScore += 1;
-            }
-
+        
+        if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
+            let roundWinner = "player";
+            console.log(roundWinner);
+            alert("You win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection + ".");
+            playerScore += 1;
+        } else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
+            let roundWinner = "computer";
+            console.log(roundWinner);
+            alert("You lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection + ".");
+            computerScore += 1;
         }
 
-        playRound(playerSelection, computerSelection);
         console.log(playerScore);
         console.log(computerScore);
 
-    }
+        isWinner(playerScore, computerScore);
+    };
 
     // Determine winner from highest score
-    let winner = "";
-    if (playerScore > computerScore) {
-        winner = "player";
-        console.log(winner);
-        alert("You win!!!");
-    } else {
-        winner = "computer";
-        console.log(winner);
-        alert("You lose!!!");
+    function isWinner (playerScore, computerScore) {
+        let winner = "";
+
+        if (playerScore === 5 || computerScore === 5) {
+
+            if (playerScore > computerScore) {
+                winner = "player";
+                console.log(winner);
+                alert("You win!!!");
+            } else {
+                winner = "computer";
+                console.log(winner);
+                alert("You lose!!!");
+            }
+
+            rockButton.removeEventListener("click", playerSelectionRock);
+            paperButton.removeEventListener("click", playerSelectionPaper);
+            scissorsButton.removeEventListener("click", playerSelectionScissors);
+        }
     }
 
-}
+};
